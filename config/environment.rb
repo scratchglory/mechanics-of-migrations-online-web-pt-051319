f04baf8b41255f11aa51f4b5cbbfba80cccf791c
scratchglory <adellakenn@gmail.com>
2019-07-29 17:58:08 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,24 @@ require 'ostruct'
 require 'date'
 
 
+
 require 'bundler/setup'
 Bundler.require
 
 
 # put the code to connect to the database here
-
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/artists.sqlite"
+  )
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
 
 require_relative "../artist.rb"
---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,25 @@
+class CreateArtists < ActiveRecord::Migration[4.2]
+
+
+ 
+ActiveRecord::Base.connection.execute(sql)
+
+    def up
+    end
+
+    def down
+    end
+
+    def change
+        create_table :artists do |t|
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometown
+        end
+    end
+    
+    
+
+
+end # end of Class
